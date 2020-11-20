d384f0bf98d442c9a139a1a6892df01392a0f387
chentsov_vs <vchencov@verci.ru>
2020-11-20 23:11:32 +0300

    Добавил рабочий скрипт по OAuth. Есть проблемы, но с ними потом разберусь

--- WebOAuth.ps1
+++ WebOAuth.ps1
@@ -1,16 +1,32 @@
-# Пример авторизации на сайте с системой Basic Authorisation 
+# Пример авторизации на сайте с системой Rest API
 # Набиваем переменные нужные для работы
+$UrlAPILogin = "https://api.pyrus.com/v4/auth"
+$URL = "http://pyrus.com/xlsx/tasklist?Mode=All&mi=15000&tz=180"
+$FileName = "TaskList_" + (Get-Date -Format ddMMyyyy) + ".xlsx"
+$path = "C:\temp"
+$login = "?Somebody_email"
+$API = "&security_key=Somebody_key"
+$AccessUrl = $UrlAPILogin + $login + $API
 
-$UrlLogin = "https://pyrus.com/ru/signup-or-login"
-$Login = "You_Login"
-$Pass = "you_Pass"
+# Тут есть как запрос задачи (легкий запрос) так и не рабочий запрос файла
+# Сложность в том, что файл генерируется при запросе, а время сессии 5 сек. 
+$TaskID = "/64706803"
+$Task = "/tasks"
+$URLTask = $UrlAPI + $Task + $TaskID
 
-# Заполняем данные защищенные данные и конвертируем. 
-$SecurePassword = $Pass `
-    | ConvertTo-SecureString -AsPlainText -Force
+$Auth = Invoke-RestMethod -Uri "$AccessUrl" -ContentType "application/json" 
+$AccessToken = $Auth.access_token
 
-$Credential = New-Object System.Management.Automation.PSCredential `
-    -ArgumentList $Login,$SecurePassword 
+$URLDownload = $UrlAPI + $URLDownload + $URLIDFile
 
-# Используем RestMethod для работы POST 
-$AccessToken = Invoke-RestMethod -Uri "$UrlLogin" -ContentType "application/json" -Credential $Credential
+$Headers = @{ 
+    "Authorization" = "Bearer $AccessToken"
+}
+
+$Logon = Invoke-RestMethod -Uri "$URLTask" -Headers $Headers -ContentType "application/json" -Method Get 
+# Следующая проблема в кодировке и разборе как добится скачивания файла.
+
+
+
+
+
