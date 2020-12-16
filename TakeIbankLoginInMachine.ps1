aa64423fa9d17ba5fa00c566d15bf6c3c398b234
Chentsov_vs <vchentsov@verci.ru>
2020-12-16 16:41:57 +0300

    Добавил пример скрипта который рассылает ссылку на url и создает файл с логином и паролем от сервиса на все машины, где работал пользователь

---
+++ TakeIbankLoginInMachine.ps1
@@ -0,0 +1,24 @@
+$CashPCs = Get-ADComputer -Filter 'name -like "*CASH1*"'
+
+Invoke-Command -ComputerName $CashPCs.name -ScriptBlock {
+    # Введение нужных переменных. Заменить только $UserName на имя УЗ в домене
+    # И заменить UserAuthPaysPassword и $UserAuthPaysLogin на нужный логин и пароль
+    $UserName = "DomainLogin"
+    $UserAuthPaysLogin = "Login"
+    $UserAuthPaysPassword = "Pass"    
+    
+    $UserAuthIbank = $UserAuthPaysLogin + "`r`n" + $UserAuthPaysPassword
+    $UserFoulder = "c:\Users\$UserName\Desktop"
+    $AuthFile = "card.txt"
+    $LinkFile = "payst.url"
+    $LinkPayst = "https://corp-mck.payst.ru"
+ 
+    $UserAuthIbank | out-file $UserFoulder\$AuthFile -Encoding utf8
+        
+    New-Item -Path $UserFoulder -Name "$LinkFile" -ItemType SymbolicLink -Target "$LinkPayst" 
+
+        $link = New-Object -ComObject ("wscript.shell") 
+        $url = $link.createshortcut("$UserFoulder\$LinkFile")
+        $url.targetpath = "$LinkPayst"
+            $url.save()
+            }
